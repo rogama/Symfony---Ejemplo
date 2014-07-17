c340b4bc294c49a15c99de7c207201ce43fdf294
Roberto <gascon87@gmail.com>
2014-07-17 21:12:05 +0200

    Ejemplo Creaccion de una pagina a la que se le pasan datos

--- src/Rogama/DemoBundle/Controller/DefaultController.php
+++ src/Rogama/DemoBundle/Controller/DefaultController.php
@@ -10,4 +10,15 @@ class DefaultController extends Controller
     {
         return $this->render('DemoBundle:Default:index.html.twig', array('name' => $name));
     }
+    
+    public function articulosAction() {
+        // Simulamos obtener los datos de la base de datos cargando los articulos a un array
+        
+        $articulos = array(
+            array('id' =>1, 'title' => 'Articulo numero 1', 'created' => '2011-01-01'),
+            array('id' =>2, 'title' => 'Articulo numero 2', 'created' => '2011-01-01'),
+            array('id' =>3, 'title' => 'Articulo numero 3', 'created' => '2011-01-01'),
+        );
+        return $this->render('DemoBundle:Default:articulos.html.twig', array('articulos'=>$articulos));
+    }
 }
