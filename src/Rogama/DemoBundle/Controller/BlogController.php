c2a05dc24b6400d3d5a17ed7ca58746e073ed127
Roberto <gascon87@gmail.com>
2014-07-18 12:48:40 +0200

    redirecciones 301 / 302

--- src/Rogama/DemoBundle/Controller/BlogController.php
+++ src/Rogama/DemoBundle/Controller/BlogController.php
@@ -9,13 +9,19 @@ use Symfony\Component\HttpFoundation\Request;
  * @author Rogama
  */
 class BlogController extends Controller{
+    public function indexAction() {
+        //Redireccion 302 (temporal)
+        return $this->redirect($this->generateUrl('homepage'));
+        //Redireccion 301 (permanente)
+        //return $this->redirect($this->generateUrl('homepage'),301);
+    }
     public function showAction($slug) {
-        //$articulo = $slug;
-        
+        //recuperar el request sin pasarlo como parametro
         $peticion = $this->getRequest();
         
-         $articulo = $peticion->get('slug'); // otra forma para obtener comodines, GET o POST    
-         $metodo = $peticion->getMethod(); //obtenemos si la petición fue por GET o POS
+        $articulo = $peticion->get('slug'); // otra forma para obtener comodines, GET o POST    
+        //$articulo = $slug;
+        $metodo = $peticion->getMethod(); //obtenemos si la petición fue por GET o POS
                  
         return $this->render('DemoBundle:Blog:show.html.twig', array('articulo'=>$articulo));
     }
