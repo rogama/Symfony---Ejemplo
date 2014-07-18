e3e45999e574d119f009dd208a3ca4cbe2ffe8c4
Roberto <gascon87@gmail.com>
2014-07-18 12:42:55 +0200

    Otra forma de obtener los valores pasados por get o post

--- src/Rogama/DemoBundle/Controller/BlogController.php
+++ src/Rogama/DemoBundle/Controller/BlogController.php
@@ -3,13 +3,18 @@
 namespace Rogama\DemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 /**
  * 
  * @author Rogama
  */
 class BlogController extends Controller{
-    public function showAction($slug) {
-        $articulo = $slug;
+    public function showAction(Request $peticion, $slug) {
+        //$articulo = $slug;
+        
+         $articulo = $peticion->get('slug'); // otra forma para obtener comodines, GET o POST    
+         $metodo = $peticion->getMethod(); //obtenemos si la petición fue por GET o POS
+                 
         return $this->render('DemoBundle:Blog:show.html.twig', array('articulo'=>$articulo));
     }
 }
