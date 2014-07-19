82b2d26daa537aba579d2341c03a78274d2d24a9
Roberto <gascon87@gmail.com>
2014-07-19 20:35:05 +0200

    registro los vendors

--- app/autoload.php
+++ app/autoload.php
@@ -7,6 +7,8 @@ use Composer\Autoload\ClassLoader;
  * @var ClassLoader $loader
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
+$loader->add('Stof', __DIR__.'/../vendor/bundles');
+$loader->add('Gedmo', __DIR__.'/../vendor/gedmo-doctrine-extensions/lib');
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
